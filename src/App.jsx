7883fcfd31ff18f86983647f341b780f0127a850
Rajveer-Singh-Git <rajveersinghbareth8@gmail.com>
2026-01-22 20:53:30 +0530

    resolved some bugs and tested running state of project

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,39 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux';
+import authService from "./appwrite/auth";
+import {login, logout} from "./store/authSlice.";
+import {Footer, Header} from "./components/index"
 
 function App() {
 
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [Loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      <h1>HI</h1>
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+          if(userData) {
+            dispatch(login(userData));
+          }
+          else {
+            dispatch(logout())
+          }
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
+  return !Loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          {/* {outlet} */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
